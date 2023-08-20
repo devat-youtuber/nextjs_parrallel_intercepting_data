114ae090b63e3af64b886a50bbf9c08edd482c5b
devat-youtuber <battuosai01@gmail.com>
2023-08-20 20:38:11 +0700

    first commit

---
+++ data.js
@@ -0,0 +1,14 @@
+export const imgUrls = [
+  'https://res.cloudinary.com/dn2htlate/image/upload/v1660537250/cld-sample.jpg',
+  'https://res.cloudinary.com/dn2htlate/image/upload/v1660537251/cld-sample-2.jpg',
+  'https://res.cloudinary.com/dn2htlate/image/upload/v1660537251/cld-sample-3.jpg',
+  'https://res.cloudinary.com/dn2htlate/image/upload/v1660537252/cld-sample-4.jpg',
+  'https://res.cloudinary.com/dn2htlate/image/upload/v1660537253/cld-sample-5.jpg',
+  'https://res.cloudinary.com/dn2htlate/image/upload/v1688609562/fashion/t0kghuelepauiw5tbsoz.jpg',
+  'https://res.cloudinary.com/dn2htlate/image/upload/v1688609561/fashion/o8eihwv8niqdmrirp5vr.png',
+  'https://res.cloudinary.com/dn2htlate/image/upload/v1660537241/samples/landscapes/landscape-panorama.jpg',
+  'https://res.cloudinary.com/dn2htlate/image/upload/v1660537237/samples/food/spices.jpg',
+  'https://res.cloudinary.com/dn2htlate/image/upload/v1660537231/samples/landscapes/architecture-signs.jpg',
+  'https://res.cloudinary.com/dn2htlate/image/upload/v1660537230/samples/animals/three-dogs.jpg',
+  'https://res.cloudinary.com/dn2htlate/image/upload/v1660537228/samples/ecommerce/shoes.png'
+]
